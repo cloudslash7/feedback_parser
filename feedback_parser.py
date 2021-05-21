27ce227d4a01bcc16da1e9456dc7bcd80fbc29e0
cloudslash7 <jedhamzawi@protonmail.com>
2021-05-21 16:10:32 -0700

    check for correct text file formatting

--- feedback_parser.py
+++ feedback_parser.py
@@ -2,19 +2,23 @@ import os
 import requests
 
 def parse_txt():
-    labels = ("title","name","date","feedback")
+    labels = ("title", "name", "date", "feedback")
     all_feedback = []
     for f in os.listdir("./"):
         if f.endswith(".txt"):
-            print(f)
             with open(f,"r") as open_file:
                 i = 0
                 feedback = {}
                 for line in open_file:
                     line = line.strip()
-                    feedback[labels[i]] = line
+                    try:
+                        feedback[labels[i]] = line
+                    except IndexError:
+                        print("{} has more than four lines. Please fix formatting and try again.".format(open_file.name))
+                        exit(1)
                     i += 1
                 all_feedback.append(feedback)
+
     return all_feedback
 
 def main():
